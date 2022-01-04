03795430896e7c8c70f92d2acaf0323515e4fda1
HC.Ji <ji.hongchao@foxmail.com>
2022-01-04 10:04:47 +0800

    Deployed 4c08633 with MkDocs version: 1.2.3

---
+++ js/baidu-tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?000a9e694095e74c47abdea7a8645fbd";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
